031020ba6511e507ec2a6314f185b753d2c2872f
Alberto Perez <alberto2perez@gmail.com>
2015-09-25 17:20:01 +0200

    fix

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,10 +10,9 @@ class squid(
    $maximum_object_size = '4096 KB',
    $ssldb_dir = '/etc/squid3/ssldb',
    $log_dir = '/var/log/squid3',
-   $configuration_options = '--sysconfdir=/etc/squid3 --mandir=/usr/share/man --enable-inline --enable-async-io=8 --enable-storeio=ufs,aufs,diskd,rock --enable-removal-policies=lru,heap --enable-delay-pools --enable-cache-digests --enable-underscores --enable-icap-client --enable-follow-x-forwarded-for --enable-eui --enable-esi --enable-ssl --enable-ssl-crtd --enable-linux-netfilter --enable-zph-qos --disable-translation --with-openssl --with-swapdir=/var/spool/squid3-cache --with-logdir=/var/log/squid3 --with-pidfile=/var/run/squid3.pid --with-filedescriptors=65536 --with-large-files --with-default-user=proxy'
 ){
 
-  $buildoptions = "--sysconfdir=/etc/squid3 --mandir=/usr/share/man --enable-inline --enable-async-io=8 --enable-storeio=ufs,aufs,diskd,rock --enable-removal-policies=lru,heap --enable-delay-pools --enable-cache-digests --enable-underscores --enable-icap-client --enable-follow-x-forwarded-for --enable-eui --enable-esi --enable-ssl --enable-ssl-crtd --enable-linux-netfilter --enable-zph-qos --disable-translation --with-openssl --with-swapdir=${cache_dir} --with-logdir=${log_dir} --with-pidfile=/var/run/squid3.pid --with-filedescriptors=65536 --with-large-files --with-default-user=proxy"
+  $build_options = "--sysconfdir=/etc/squid3 --mandir=/usr/share/man --enable-inline --enable-async-io=8 --enable-storeio=ufs,aufs,diskd,rock --enable-removal-policies=lru,heap --enable-delay-pools --enable-cache-digests --enable-underscores --enable-icap-client --enable-follow-x-forwarded-for --enable-eui --enable-esi --enable-ssl --enable-ssl-crtd --enable-linux-netfilter --enable-zph-qos --disable-translation --with-openssl --with-swapdir=${cache_dir} --with-logdir=${log_dir} --with-pidfile=/var/run/squid3.pid --with-filedescriptors=65536 --with-large-files --with-default-user=proxy"
   $user = 'proxy'
   $group = 'proxy'
 
@@ -55,7 +54,7 @@ class squid(
 
     exec { "configure":
       cwd     => "/tmp/squid*",
-      command => "./configure ${configuration_options}",
+      command => "./configure ${build_options}",
       after => 'uncompress',
     } 
 
