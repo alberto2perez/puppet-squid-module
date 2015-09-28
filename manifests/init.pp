7dd01d8a3fff5bd1e43a63cb24c2d22539c8ff9d
Alberto Perez <alberto2perez@gmail.com>
2015-09-28 15:44:28 +0200

    fix

--- manifests/init.pp
+++ manifests/init.pp
@@ -70,7 +70,7 @@ class squid(
         owner   => 'root',
         group   => 'root',
         mode    => '0644',
-        content => template("squid/templates/squid.conf.erb"),
+        content => template("squid/squid.conf.erb"),
         require => 'squid3',
     }
 
